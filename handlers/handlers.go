a895c95609a6dd7205597a80876707d56253341a
x13relay <x13bitnode@gmail.com>
2026-05-24 00:15:03 +0300

    HTTP handlers layer + сборка в main

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,7 +1,9 @@
 package handlers
 
 import (
+	"OilStore/models"
 	"OilStore/postgresql"
+	"encoding/json"
 	"net/http"
 )
 
@@ -16,5 +18,27 @@ func NewHandlers(oilConn *postgresql.OilConn) *Handlers {
 }
 
 func (h *Handlers) AddOil(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Wrong HTTP method!", http.StatusBadRequest)
+		return
+	}
+	var newOil models.Oil
 
+	errJson := json.NewDecoder(r.Body).Decode(&newOil)
+	if errJson != nil {
+		http.Error(w, "Can't read jSon body", http.StatusBadRequest)
+		return
+	}
+	id, errDB := h.oilConn.AddOil(r.Context(), newOil)
+	if errDB != nil {
+		http.Error(w, "Fail to write DB", 500)
+		return
+	}
+	response := map[string]interface{}{
+		"id":      id,
+		"message": "Succes!New Oil created!",
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(response)
 }
--- main.go
+++ main.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"OilStore/handlers"
+	"OilStore/postgresql"
+	"context"
+	"fmt"
+	"net/http"
+)
+
 func main() {
+	ctx := context.Background()
+	conn, errCon := postgresql.ConnectionBD_oil(ctx)
+	if errCon != nil {
+		fmt.Println("Ошибка подключения к БД", errCon)
+		return
+	}
+	oilConn := postgresql.NewOilConn(conn)
+	handlers := handlers.NewHandlers(oilConn)
+	mux := http.NewServeMux()
+	errBD := oilConn.CreateTableOils(ctx)
+	if errBD != nil {
+		fmt.Println("Ошибка при создании таблицы в БД", errBD)
+		return
+	}
+	fmt.Println("БД готова")
+	mux.HandleFunc("/oils", handlers.AddOil)
+	fmt.Println("Сервер запущен. Жду запросы на :8080")
+	errServer := http.ListenAndServe(":8080", mux)
+	if errServer != nil {
+		fmt.Println("Ошибка сервера!", errServer)
+		return
+	}
 
 }
