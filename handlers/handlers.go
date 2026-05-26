419bca8ba97799141860fb5e4c0494b59feb6bd0
x13relay <x13bitnode@gmail.com>
2026-05-27 00:55:14 +0300

    тест-запуски на 4 хендлерах

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -5,6 +5,7 @@ import (
 	"OilStore/postgresql"
 	"encoding/json"
 	"net/http"
+	"strconv"
 )
 
 type Handlers struct {
@@ -41,4 +42,103 @@ func (h *Handlers) AddOil(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(response)
+}
+
+func (h *Handlers) DeleteOilById(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodDelete {
+		http.Error(w, "Wrong HTTP method!", http.StatusBadRequest)
+		return
+	}
+
+	idStr := r.PathValue("id")
+
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "incorrect ID value!", http.StatusBadRequest)
+		return
+	}
+	errBD := h.oilConn.DeleteOilById(r.Context(), id)
+	if errBD != nil {
+		http.Error(w, "oil not found"+errBD.Error(), http.StatusNotFound)
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
+
+}
+
+func (h *Handlers) FullUpdateOil(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPatch {
+		http.Error(w, "wrong HTTP method!", http.StatusBadRequest)
+		return
+	}
+	var updateOil models.Oil
+
+	idStr := r.PathValue("id")
+	id, errId := strconv.Atoi(idStr)
+	if errId != nil {
+		http.Error(w, "incorrect ID value!", http.StatusBadRequest)
+		return
+	}
+
+	errJson := json.NewDecoder(r.Body).Decode(&updateOil)
+	if errJson != nil {
+		http.Error(w, "Can't read jSon body"+errJson.Error(), http.StatusBadRequest)
+		return
+	}
+
+	updOil, errBD := h.oilConn.FullUpdateOil(r.Context(), updateOil, id)
+	if errBD != nil {
+		http.Error(w, "Database write error: oil data not updated!"+errBD.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	errJREQ := json.NewEncoder(w).Encode(updOil)
+	if errJREQ != nil {
+		http.Error(w, "response error"+errJREQ.Error(), http.StatusInternalServerError)
+		return
+	}
+
+}
+
+func (h *Handlers) GetMinMaxOil(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "wrong HTTP method!", http.StatusBadRequest)
+		return
+	}
+
+	query := r.URL.Query()
+
+	qMin := query.Get("min")
+	qMax := query.Get("max")
+
+	if qMin == "" || qMax == "" {
+		http.Error(w, "incorrect minimum or maximum price", http.StatusBadRequest)
+		return
+	}
+
+	min, errMin := strconv.Atoi(qMin)
+
+	if errMin != nil {
+		http.Error(w, "incorrect min price!", http.StatusBadRequest)
+		return
+	}
+
+	max, errMax := strconv.Atoi(qMax)
+	if errMax != nil {
+		http.Error(w, "incorrect max price", http.StatusBadRequest)
+		return
+	}
+	oils, errBD := h.oilConn.GetMinMaxOil(r.Context(), min, max)
+	if errBD != nil {
+		http.Error(w, "data base error!", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(oils)
+	w.WriteHeader(http.StatusOK)
+
 }
--- main.go
+++ main.go
@@ -23,9 +23,12 @@ func main() {
 		fmt.Println("Ошибка при создании таблицы в БД", errBD)
 		return
 	}
-	fmt.Println("БД готова")
-	mux.HandleFunc("/oils", handlers.AddOil)
-	fmt.Println("Сервер запущен. Жду запросы на :8080")
+	fmt.Println("БД готова. Сервер запущен. Жду запросы на :8080")
+	mux.HandleFunc("POST /oils", handlers.AddOil)
+	mux.HandleFunc("DELETE /del/{id}", handlers.DeleteOilById)
+	mux.HandleFunc("PATCH /oils/{id}", handlers.FullUpdateOil)
+	mux.HandleFunc("GET /oils", handlers.GetMinMaxOil)
+
 	errServer := http.ListenAndServe(":8080", mux)
 	if errServer != nil {
 		fmt.Println("Ошибка сервера!", errServer)
